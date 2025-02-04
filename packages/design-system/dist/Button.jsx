9f5d32037b0892b4aa5dd1f0c621678f23250295
Steffen <steffen.kotschi@outlook.com>
2025-02-04 21:15:56 +0000

    Update

---
+++ packages/design-system/dist/Button.jsx
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = void 0;
+var react_1 = __importDefault(require("react"));
+var Button = function (_a) {
+    var children = _a.children, onClick = _a.onClick;
+    return (<button onClick={onClick} style={{
+            padding: '8px 16px',
+            backgroundColor: '#0070f3',
+            color: 'white',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+        }}>
+      {children}
+    </button>);
+};
+exports.Button = Button;
